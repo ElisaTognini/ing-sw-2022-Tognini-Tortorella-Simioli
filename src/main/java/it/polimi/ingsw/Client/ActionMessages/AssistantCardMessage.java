c6b22443a993e09e7be936c810e44324768fb60a
emiliasimioli <emilia.simioli@mail.polimi.it>
2022-06-14 16:42:24 +0200

    comments on Client classes (ActionMessages package - AssistantCardMessage)

--- src/main/java/it/polimi/ingsw/Client/ActionMessages/AssistantCardMessage.java
+++ src/main/java/it/polimi/ingsw/Client/ActionMessages/AssistantCardMessage.java
@@ -2,13 +2,32 @@ package it.polimi.ingsw.Client.ActionMessages;
 
 import java.io.Serializable;
 
+/**
+ * Class AssistantCardMessage is a message sent from the client to the server,
+ * informing it of the chosen assistant card.
+ *
+ * @see java.io.Serializable
+ * */
+
 public class AssistantCardMessage implements Serializable {
 
     int pickedCardID;
 
+    /**
+     * Constructor AssistantCardMessage creates a new AssistantCardMessage instance.
+     *
+     * @param cardID - of type int - assistant card ID reference.
+     * */
+
     public AssistantCardMessage(int cardID){
         this.pickedCardID = cardID;
     }
+
+    /**
+     * Getter method getPickedCardID returns the card ID of this AssistantCardMessage object.
+     *
+     * @return the card ID (type int) of this AssistantCardMessage object.
+     * */
 
     public int getPickedCardID(){
         return pickedCardID;
