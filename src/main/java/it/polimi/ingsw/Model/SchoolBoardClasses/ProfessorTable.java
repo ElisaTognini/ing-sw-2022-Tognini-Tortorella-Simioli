5b6554495738f430e5455f409179f1bdae3eeaca
maratortorella <100294544+maratortorella@users.noreply.github.com>
2022-06-19 19:16:01 +0200

    progress on commenting: finished package BoardClasses, fully commented Player, StudentContainer and part of the SchoolBoardClasses package (DiningRoom, Entrance and ProfessorTable)

--- src/main/java/it/polimi/ingsw/Model/SchoolBoardClasses/ProfessorTable.java
+++ src/main/java/it/polimi/ingsw/Model/SchoolBoardClasses/ProfessorTable.java
@@ -3,18 +3,28 @@ package it.polimi.ingsw.Model.SchoolBoardClasses;
 import it.polimi.ingsw.Utils.Enums.PawnDiscColor;
 import it.polimi.ingsw.Model.BasicElements.Professor;
 
+/** Class ProfessorTable is one of the classes which compose the player's school board: it contains two arrays, one
+  * which stores the professors when and as the player get control of them and the other one is used in expert mode
+  * when character card 2 is played to store the state of the professor table before the player decides which
+  * professor to take control of.*/
 public class ProfessorTable {
 
     private Professor[] professors;
     private Professor[] previousState;
 
+    /** Constructor ProfessorTable creates a new instance of professor table and initializes the professors */
     public ProfessorTable() {
-
         this.professors = new Professor[5];
         initializeProfessors();
 
     }
 
+
+    /** Method addProfessor adds the professor of the selected color to the professor table and calls method of
+     * professor to signal that this professor is owned by a player.
+     *
+     * @param color of type PawnDiscColor - color of the professor
+     * */
     public void addProfessor(PawnDiscColor color) {
         for (Professor p : professors) {
             if (p.getColor().equals(color)) {
@@ -23,6 +33,12 @@ public class ProfessorTable {
         }
     }
 
+
+    /** Method removeProfessor removes the professor of the selected color from the professor table and calls method
+     * of professor to signal that this professor is owned by a player.
+     *
+     * @param color of type PawnDiscColor - color of the professor
+     * */
     public void removeProfessor(PawnDiscColor color) {
         for (Professor p : professors) {
             if (p.getColor().equals(color)) {
@@ -31,6 +47,10 @@ public class ProfessorTable {
         }
     }
 
+    /** Method hasProfessor checks if the professor of selected color is owned by a player
+     *
+     * @param color of type PawnDiscColor - color of the professor
+     * */
     public boolean hasProfessor(PawnDiscColor color) {
         for (Professor p : professors) {
             if (p.getColor().equals(color)) {
@@ -40,6 +60,8 @@ public class ProfessorTable {
         return false;
     }
 
+    /** Private method initializeProfessors creates a new Professor for each value of the PawnDiscColor enum
+     * and sets value of attribute ownedByPlayer to false by default. */
     private void initializeProfessors() {
         int i = 0;
         for (PawnDiscColor c : PawnDiscColor.values()) {
@@ -49,6 +71,9 @@ public class ProfessorTable {
         }
     }
 
+    /** Method getNumberOfProfessors returns the number of professors owned by a player
+     *
+     * @return int - number of professors owned */
     public int getNumberOfProfessors() {
         int count = 0;
         for (Professor p : professors) {
@@ -58,8 +83,10 @@ public class ProfessorTable {
         return count;
     }
 
-    @Override
-    /* FORMAT only PawnDiscColor of owned professors */
+    /** Method toString builds a String containing all the info stored in this class
+     *
+     *  @return String - FORMAT: the professors owned by the player, each separated by a blank space.
+     *  */
     public String toString() {
         StringBuilder toRet = new StringBuilder();
 
@@ -71,7 +98,8 @@ public class ProfessorTable {
         return toRet.toString();
     }
 
-    /* this method keeps track of the professors the player had before playing card 2 */
+    /** Method saveProfessor keeps track of the professors the player had before playing character card 2
+     * and saves it in array previousState */
     public void saveProfessors() {
         previousState = new Professor[professors.length];
         int i = 0;
@@ -83,7 +111,8 @@ public class ProfessorTable {
         }
     }
 
-    /* this method resets the player's professor table to the state it was before playing card 2*/
+    /** Method resetPreviousProfessorTable resets the player's professor table to the state it was before playing
+     * character card 2*/
     public void resetPreviousProfessorTable(){
         professors = previousState;
     }
