f0da65e4ebbedc8cac6bd1d824d902d8177b70ad
emiliasimioli <emilia.simioli@mail.polimi.it>
2022-06-28 15:41:16 +0200

    finished comments!

--- src/main/java/it/polimi/ingsw/View/GUI/StudentListSelectionController.java
+++ src/main/java/it/polimi/ingsw/View/GUI/StudentListSelectionController.java
@@ -7,6 +7,10 @@ import javafx.scene.image.ImageView;
 
 import java.util.ArrayList;
 
+/**
+ * Class StudentListSelectionController is used to add selected students to an ArrayList, when using a character card
+ * that requires it, in order to swap students between two places.
+ * */
 public class StudentListSelectionController {
 
     @FXML ImageView blueStudent;
@@ -18,31 +22,49 @@ public class StudentListSelectionController {
 
     ArrayList<PawnDiscColor> colors = new ArrayList<>();
 
+    /**
+     * Method addYellowStudent adds a yellow student to the student ArrayList.
+     * */
     public void addYellowStudent(){
         colors.add(PawnDiscColor.YELLOW);
         checkForSize();
     }
 
+    /**
+     * Method addPinkStudent adds a pink student to the student ArrayList.
+     * */
     public void addPinkStudent(){
         colors.add(PawnDiscColor.PINK);
         checkForSize();
     }
 
+    /**
+     * Method addGreenStudent adds a green student to the student ArrayList.
+     * */
     public void addGreenStudent(){
         colors.add(PawnDiscColor.GREEN);
         checkForSize();
     }
 
+    /**
+     * Method addRedStudent adds a red student to the student ArrayList.
+     * */
     public void addRedStudent(){
         colors.add(PawnDiscColor.RED);
         checkForSize();
     }
 
+    /**
+     * Method addBlueStudent adds a blue student to the student ArrayList.
+     * */
     public void addBlueStudent(){
         colors.add(PawnDiscColor.BLUE);
         checkForSize();
     }
 
+    /**
+     * Method checkForSize is used to ensures that the student ArrayList does not contain more that three students.
+     * */
     private void checkForSize(){
         if(colors.size() >= 3){
             if(MainGUIController.getParameter().getColorArrayList() == null){
@@ -55,6 +77,9 @@ public class StudentListSelectionController {
         }
     }
 
+    /**
+     * Method doneClicked closes the selection stage when "done" button is clicked.
+     * */
     public void doneClicked(){
         if(MainGUIController.getParameter().getColorArrayList() == null){
             MainGUIController.getParameter().setColorArrayList(colors);
--- src/main/java/it/polimi/ingsw/View/GUI/StudentSelectionController.java
+++ src/main/java/it/polimi/ingsw/View/GUI/StudentSelectionController.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
+/**
+ * Class StudentSelectionController is used to select student colors to add as parameters
+ * when some character cards are used.
+ * */
 public class StudentSelectionController {
 
     @FXML ImageView blueStudent;
@@ -14,28 +18,50 @@ public class StudentSelectionController {
     @FXML ImageView yellowStudent;
     @FXML Button doneButton;
 
+    /**
+     * Method selectBlueStudent detects when the blue student is clicked, passing it as parameter to the character card.
+     * */
     public void selectBlueStudent(){
         MainGUIController.getParameter().setColor(PawnDiscColor.BLUE);
         doneButton.setVisible(true);
 
     }
+
+    /**
+     * Method selectRedStudent detects when the red student is clicked, passing it as parameter to the character card.
+     * */
     public void selectRedStudent(){
         MainGUIController.getParameter().setColor(PawnDiscColor.RED);
         doneButton.setVisible(true);
     }
+
+    /**
+     * Method selectPinkStudent detects when the pink student is clicked, passing it as parameter to the character card.
+     * */
     public void selectPinkStudent(){
         MainGUIController.getParameter().setColor(PawnDiscColor.PINK);
         doneButton.setVisible(true);
     }
+
+    /**
+     * Method selectGreenStudent detects when the green student is clicked, passing it as parameter to the character card.
+     * */
     public void selectGreenStudent(){
         MainGUIController.getParameter().setColor(PawnDiscColor.GREEN);
         doneButton.setVisible(true);
-
     }
+
+    /**
+     * Method selectYellowStudent detects when the yellow student is clicked, passing it as parameter to the character card.
+     * */
     public void selectYellowStudent(){
         MainGUIController.getParameter().setColor(PawnDiscColor.YELLOW);
         doneButton.setVisible(true);
     }
+
+    /**
+     * Method doneClicked closes the selection stage when "done" button is clicked.
+     * */
     public void doneClicked(){
         MainGUIController.closeCardStage();
     }
